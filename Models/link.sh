b2713dc64cd22ba5b69bcbd3f85f54993f836aed
Joseph Mearman <joseph@mearman.co.uk>
2023-06-12 04:15:56 +0100

    Create symbolic links for files in source_dir in multiple target_dirs. Fixes potential issue where duplicate file would not be symlinked.

--- Models/link.sh
+++ Models/link.sh
@@ -16,30 +16,14 @@ done
 for file in "$source_dir"/*; do
 	echo "Processing $file"
 	if [[ -f "$file" ]]; then
-		echo "Creating symbolic links for $file"
 		# Get the file name from the full path
 		file_name=$(basename "$file")
-
 		# Create a symbolic link in each target directory
 		for target_dir in "${target_dirs[@]}"; do
 			echo "Creating symbolic link in $target_dir"
-			if [[ -f "$target_dir/$file_name" ]]; then
-				echo "File $target_dir/$file_name already exists"
-				# check if file is already symlink
-				if [[ -L "$target_dir/$file_name" ]]; then
-					echo "File $target_dir/$file_name is a symlink, removing"
-					rm "$target_dir/$file_name"
-				else
-					echo "File $target_dir/$file_name is not a symlink, skipping"
-					continue
-				fi
-			else
-				echo "File $target_dir/$file_name does not exist"
-			fi
+
 			ln -s "$file" "$target_dir/$file_name"
 		done
-	else
-		echo "$file is not a file, skipping"
 	fi
 	echo
 done
