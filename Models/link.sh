8e3e0fd527a6077be26107c6b3d2103193e1ee26
Joseph Mearman <joseph@mearman.co.uk>
2023-06-12 04:19:03 +0100

    Add error messages when target directory or file not found in Models/link.sh.

--- Models/link.sh
+++ Models/link.sh
@@ -13,18 +13,36 @@ for target_dir in "${target_dirs[@]}"; do
 done
 
 # Iterate over the files in the source directory
+# Loop through the files in the source directory
 for file in "$source_dir"/*; do
-	echo "Processing $file"
-	if [[ -f "$file" ]]; then
-		# Get the file name from the full path
-		file_name=$(basename "$file")
-		# Create a symbolic link in each target directory
-		for target_dir in "${target_dirs[@]}"; do
-			echo "Creating symbolic link in $target_dir"
-
-			ln -s "$file" "$target_dir/$file_name"
-		done
+	# Make sure the file exists
+	if [[ ! -f "$file" ]]; then
+		echo "Error: $file does not exist" >&2
+		continue
 	fi
+	# Get the file name from the full path
+	file_name=$(basename "$file")
+	# Create a symbolic link in each target directory
+	for target_dir in "${target_dirs[@]}"; do
+		# Make sure the target directory exists
+		if [[ ! -d "$target_dir" ]]; then
+			echo "Error: $target_dir does not exist" >&2
+			continue
+		fi
+		# Check if a symbolic link already exists
+		if [[ -L "$target_dir/$file_name" ]]; then
+			echo "Warning: symbolic link already exists in $target_dir" >&2
+			continue
+		fi
+		# Check if a file with the same name already exists
+		if [[ -e "$target_dir/$file_name" ]]; then
+			echo "Error: file with the same name already exists in $target_dir" >&2
+			continue
+		fi
+		# Create the symbolic link
+		echo "Creating symbolic link in $target_dir"
+		ln -s "$file" "$target_dir/$file_name"
+	done
 	echo
 done
 
