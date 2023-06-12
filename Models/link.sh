eb1c4486423578b765370e4d096eda9269e7d94a
Joseph Mearman <joseph@mearman.co.uk>
2023-06-12 04:53:48 +0100

    Set default values for variables and create env file for LINK_SOURCE_DIR and LINK_TARGET_DIRS.

--- Models/link.sh
+++ Models/link.sh
@@ -1,77 +1,70 @@
 #!/bin/bash
 set -euo pipefail
 
-# Load environment variables from file if no command line arguments are passed
-if [[ $# -eq 0 ]]; then
-	if [ -f sample.env ]; then
-		source sample.env
-	else
-		echo "Error: sample.env file not found" >&2
-		echo "Creating env file from sample.env"
-		cp sample.env env
-	fi
-else
-	# Parse command line arguments
-	while [[ $# -gt 0 ]]; do
-		key="$1"
-		case $key in
-		-s | --source-dir)
-			SOURCE_DIR="$2"
-			shift
-			shift
-			;;
-		-t | --target-dir)
-			TARGET_DIRS+=("$2")
-			shift
-			shift
-			;;
-		-f | --force)
-			FORCE=true
-			shift
-			;;
-		*)
-			echo "Unknown option: $1" >&2
-			exit 1
-			;;
-		esac
-	done
-fi
+# Set default values for environment variables
+LINK_SOURCE_DIR="${LINK_SOURCE_DIR:-}"
+LINK_TARGET_DIRS=("${LINK_TARGET_DIRS[@]:-}")
+
+# Parse command line arguments
+while [[ $# -gt 0 ]]; do
+	key="$1"
+	case $key in
+	-s | --source-dir)
+		LINK_SOURCE_DIR="$2"
+		shift
+		shift
+		;;
+	-t | --target-dir)
+		LINK_TARGET_DIRS+=("$2")
+		shift
+		shift
+		;;
+	-f | --force)
+		FORCE=true
+		shift
+		;;
+	*)
+		echo "Unknown option: $1" >&2
+		exit 1
+		;;
+	esac
+done
 
 # Check if source directory is specified
-if [[ -z "${SOURCE_DIR:-}" ]]; then
-	SOURCE_DIR="${LINK_SOURCE_DIR:-}"
-fi
-
-if [[ -z "${SOURCE_DIR:-}" ]]; then
+if [[ -z "${LINK_SOURCE_DIR:-}" ]]; then
 	echo "Error: source directory not specified" >&2
 	exit 1
 fi
 
 # Check if target directories are specified
-if [[ ${#TARGET_DIRS[@]} -eq 0 ]]; then
-	TARGET_DIRS=(${LINK_TARGET_DIRS:-})
-fi
-
-if [[ ${#TARGET_DIRS[@]} -eq 0 ]]; then
+if [[ ${#LINK_TARGET_DIRS[@]} -eq 0 ]]; then
 	echo "Error: target directories not specified" >&2
 	exit 1
 fi
 
+# Create the .env file
+echo "# Comment out the source and targets in the .env file" >.env
+echo "LINK_SOURCE_DIR=" >>.env
+echo "LINK_TARGET_DIRS=(" >>.env
+for target_dir in "${LINK_TARGET_DIRS[@]}"; do
+	echo "\"$target_dir\"" >>.env
+done
+echo ")" >>.env
+
 # Create the target directories if they don't exist
-for target_dir in "${TARGET_DIRS[@]}"; do
+for target_dir in "${LINK_TARGET_DIRS[@]}"; do
 	mkdir -p "$target_dir" || echo "$target_dir already exists"
 done
 
 # Iterate over the files in the source directory
-# Loop through the files in the source directory
-for target_dir in "${TARGET_DIRS[@]}"; do
+for target_dir in "${LINK_TARGET_DIRS[@]}"; do
 	# Make sure the target directory exists
 	if [[ ! -d "$target_dir" ]]; then
 		echo "Error: $target_dir does not exist" >&2
 		continue
 	fi
 	# Create a symbolic link in each target directory
-	for file in "$SOURCE_DIR"/*; do
+	for file in "$LINK_SOURCE_DIR"/*; do
 		echo
 		# Make sure the file exists
 		if [[ ! -f "$file" ]]; then
